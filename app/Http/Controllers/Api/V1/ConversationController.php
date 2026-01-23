37cf3fa58b0b4bb5047a3ef02a863f8092eb600a
Decloedt-Cloud <116311774+Decloedt-Cloud@users.noreply.github.com>
2026-01-23 18:10:18 +0100

    Update ConversationController.php

--- app/Http/Controllers/Api/V1/ConversationController.php
+++ app/Http/Controllers/Api/V1/ConversationController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Str;
 
 class ConversationController extends Controller
 {
